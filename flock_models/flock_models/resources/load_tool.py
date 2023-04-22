c9aa9c994528c30f7107f771e7b81648333a6917
Adam Yodinsky <27074934+adamyodinsky@users.noreply.github.com>
2023-04-22 22:20:52 +0300

    loader tool

---
+++ flock_models/flock_models/resources/load_tool.py
@@ -0,0 +1,61 @@
+"""Resource for vectorstore."""
+
+from typing import Any
+
+from flock_schemas import LoadToolSchema
+from flock_schemas.base import Kind
+from langchain.agents import load_tools as load_toolsLC
+from langchain.schema import BaseLanguageModel as LCBaseLanguageModel
+from langchain.tools.base import BaseTool
+
+from flock_models.resources.base import ToolResource
+
+
+class LoadToolResource(ToolResource):
+    """Class for vectorstore qa tool."""
+
+    VENDORS = [
+        "python_repl"
+        "requests"
+        "requests_get"
+        "requests_post"
+        "requests_patch"
+        "requests_put"
+        "requests_delete"
+        "terminal"
+        "wolfram-alpha"
+        "google-search"
+        "google-search-results-json"
+        "searx-search-results-json"
+        "bing-search"
+        "google-serper"
+        "serpapi"
+        "searx-search"
+        "wikipedia"
+        "human"
+        "news-api"
+        "tmdb-api"
+        "podcast-api"
+        "pal-math"
+        "pal-colored-objects"
+        "llm-math"
+        "open-meteo-api"
+    ]
+
+    def __init__(
+        self,
+        manifest: LoadToolSchema,
+        dependencies: dict[str, Any],
+        tools: list[Any] = [],
+    ):
+        super().__init__(manifest, dependencies)
+        self.vendor_cls: BaseTool = self.vendor
+        self.llm: LCBaseLanguageModel = self.dependencies[Kind.LLM]
+
+        self.tool_function = load_toolsLC(
+            tool_names=[self.vendor],
+            llm=self.llm,
+            **self.options,
+        )[0]
+
+        self.resource = self.tool_function
