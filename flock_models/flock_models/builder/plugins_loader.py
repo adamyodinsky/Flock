426d87bcf076dbb27ca1b5806ad74fdc9f2d721a
Adam Yodinsky <27074934+adamyodinsky@users.noreply.github.com>
2023-04-24 18:25:07 +0300

    working on typed python. getting it ready for compliation

--- flock_models/flock_models/builder/plugins_loader.py
+++ flock_models/flock_models/builder/plugins_loader.py
@@ -1,8 +1,11 @@
+"""Load plugins from plugins directory."""
+
 import os
 import importlib
 from flock_models.resources.base import Resource
 
-def __load_plugins(plugin_directory: str, plugin_base_class = Resource) -> dict:
+
+def __load_plugins(plugin_directory: str, plugin_base_class=Resource) -> dict:
     plugins_map = {}
 
     # if plugin_directory not exist return empty dict
@@ -17,19 +20,25 @@ def __load_plugins(plugin_directory: str, plugin_base_class = Resource) -> dict:
             for item in dir(module):
                 obj = getattr(module, item)
 
-                if isinstance(obj, type) and issubclass(obj, plugin_base_class) and obj != plugin_base_class:
+                if (
+                    isinstance(obj, type)
+                    and issubclass(obj, plugin_base_class)
+                    and obj != plugin_base_class
+                ):
                     plugins_map[module_name] = obj
     return plugins_map
 
 
 def load_plugins(native_plugins_dir: str = "plugins") -> dict:
-        external_plugins_path: str = os.getenv("FLOCK_PLUGINS_PATH")
-        if external_plugins_path:
-            external_plugins: dict = __load_plugins(external_plugins_path)
-        else :
-            external_plugins: dict = {}
+    """Load plugins from plugins directory."""
 
-        native_plugins: dict = __load_plugins(plugin_directory=native_plugins_dir)
-        plugins: dict = {**external_plugins, **native_plugins}
+    external_plugins_path: str = os.getenv("FLOCK_PLUGINS_PATH", "")
+    if external_plugins_path:
+        external_plugins: dict = __load_plugins(external_plugins_path)
+    else:
+        external_plugins: dict = {}
 
-        return plugins
+    native_plugins: dict = __load_plugins(plugin_directory=native_plugins_dir)
+    plugins: dict = {**external_plugins, **native_plugins}
+
+    return plugins
