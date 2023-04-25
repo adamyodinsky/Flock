c0529b59567d3fd62c054eac4738051b6cafc362
Adam Yodinsky <27074934+adamyodinsky@users.noreply.github.com>
2023-04-25 14:38:37 +0300

    working on typed python. getting it ready for compliation

--- flock_schemas/flock_schemas/splitter.py
+++ flock_schemas/flock_schemas/splitter.py
@@ -5,7 +5,7 @@ from typing import Literal
 
 from pydantic import Field
 
-from flock_schemas.base import BaseFlockSchema, BaseOptions, Kind
+from flock_schemas.base import BaseFlockSchema, BaseOptions
 
 
 class SplitterVendor(str, Enum):
