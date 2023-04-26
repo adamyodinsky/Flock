ea925887df14a32c52a3505de9f5297ffef52ebe
Adam Yodinsky <27074934+adamyodinsky@users.noreply.github.com>
2023-04-27 02:06:20 +0300

    working on typed python. getting it ready for compliation

--- flock_resource_store/flock_resource_store/__init__.py
+++ flock_resource_store/flock_resource_store/__init__.py
@@ -1,13 +1,11 @@
 from flock_resource_store.base import ResourceStore
-from flock_resource_store.etcd import EtcdResourceStore
 from flock_resource_store.fs import FSResourceStore
+
 
 class ResourceStoreFactory:
     @staticmethod
     def get_resource_store(store_type: str, key_prefix: str) -> ResourceStore:
-        if store_type == "etcd":
-            return EtcdResourceStore(key_prefix)
-        elif store_type == "fs":
+        if store_type == "fs":
             return FSResourceStore(key_prefix)
         else:
             raise ValueError(f"Invalid resource store type: {store_type}")
