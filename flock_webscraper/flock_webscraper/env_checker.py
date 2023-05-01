81d885a0bcfe963e084d6af3c09995277d473068
Adam Yodinsky <27074934+adamyodinsky@users.noreply.github.com>
2023-05-01 10:24:01 +0300

    web scraper

---
+++ flock_webscraper/flock_webscraper/env_checker.py
@@ -0,0 +1,22 @@
+import os
+
+
+class EnvVarNotSetError(Exception):
+    """Raised when an environment variable is not set"""
+
+    def __init__(self, var_name):
+        self.var_name = var_name
+        super().__init__(f"Environment variable '{self.var_name}' is not set")
+
+
+def check_env_vars(required_vars, optional_vars=None):
+    """Check that all required environment variables are set"""
+
+    for var in required_vars:
+        if var not in os.environ:
+            raise EnvVarNotSetError(var)
+
+    if optional_vars:
+        for var in optional_vars:
+            if var not in os.environ:
+                print(f"Warning: Optional environment variable '{var}' is not set")
