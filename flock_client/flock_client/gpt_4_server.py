43a002ef4e800075a26c7343b58ed75df0e95089
Adam Yodinsky <27074934+adamyodinsky@users.noreply.github.com>
2023-05-17 00:57:58 +0300

    llm local gpt4all working - let's api it

---
+++ flock_client/flock_client/gpt_4_server.py
@@ -0,0 +1,64 @@
+import os
+from typing import Dict, List, Optional
+
+from fastapi import FastAPI
+from gpt4all import GPT4All
+from pydantic import BaseModel
+from uvicorn import run
+
+
+class Request(BaseModel):
+    messages: List[Dict]
+    model: str
+    temperature: Optional[float]
+    top_p: Optional[float]
+    top_k: Optional[int]
+    max_tokens: Optional[int]
+    batch: Optional[int]
+    repeat_penalty: Optional[float]
+    repeat_penalty_tokens: Optional[int]
+
+
+class Response(BaseModel):
+    object: str
+    model: str
+    choices: List[Dict]
+    usage: Dict
+
+
+gpt = GPT4All(
+    model_name="ggml-gpt4all-l13b-snoozy",
+    model_path="/Users/adamyodinsky/.flock/models/",
+    model_type="llama",
+)
+
+app = FastAPI()
+
+
+@app.post("/v1/chat/completions")
+async def agent_endpoint(req: Request):
+    """Endpoint for the agent to call for inference."""
+
+    answer = gpt.chat_completion(
+        messages=req.messages
+    )  # **req.dict(exclude={"messages"}
+
+    try:
+        bot_message = answer.get("choices", [{}])[0].get("message", {}).get("content", "")  # type: ignore
+    except Exception as error:
+        bot_message = str(error)
+
+    return Response(
+        object="chat.completion",
+        model=req.model,
+        choices=[{"message": {"role": "assistant", "content": bot_message}}],
+        usage={"prompt_tokens": 0, "completion_tokens": 0, "total_tokens": 0},
+    )
+
+
+host = os.environ.get("FLOCK_GPT4ALL_HOST", "localhost")
+port = int(os.environ.get("FLOCK_GPT4ALL_PORT", "8080"))
+
+print(f"Starting server on {host}:{port}")
+
+run(app, host=host, port=port, log_level="warning")
---
+++ flock_client/flock_client/reponses.py
@@ -0,0 +1,94 @@
+"""This module contains the function that is called when the user presses submit."""
+
+import os
+
+import requests
+from gpt4all import GPT4All
+
+gpt = GPT4All(
+    model_name="ggml-gpt4all-l13b-snoozy",
+    model_path="/Users/adamyodinsky/.flock/models/",
+    model_type="llama",
+)
+
+
+class Responder:
+    """This class contains the function that is called when the user presses submit."""
+
+    def __init__(self, backend, backend_type, model) -> None:
+        self.backend = backend
+        self.backend_type = backend_type
+        self.model = model
+
+    def get_agent_responder(self):
+        def responder(user_message, chat_history):
+            """This function is called when the user presses submit."""
+
+            try:
+                response = requests.post(
+                    self.backend,
+                    json={"msg": user_message},
+                    timeout=None,
+                ).json()
+                bot_message = response["data"]
+            except requests.exceptions.Timeout as error:
+                bot_message = f"{str(error)}"
+
+            chat_history.append((user_message, bot_message))
+            return "", chat_history
+
+        return responder
+
+    def get_llama_responder(self):
+        def responder(user_message, chat_history):
+            """This function is called when the user presses submit."""
+
+            messages = self._tuple_to_dict(chat_history)
+            messages.append({"role": "user", "content": user_message})
+
+            try:
+                response = requests.post(
+                    self.backend,
+                    json={
+                        "messages": messages,
+                        "model": self.model,
+                        "temperature": 0.28,
+                        "top_p": 0.95,
+                        "top_k": 40,
+                        "max_tokens": 4096,
+                        "batch": 9,
+                        "repeat_penalty": 1.1,
+                        "repeat_penalty_tokens": 60,
+                    },
+                    timeout=None,
+                ).json()
+                bot_message = (
+                    response.get("choices", [{}])[0]
+                    .get("message", {})
+                    .get("content", "")
+                )
+            except requests.exceptions.Timeout as error:
+                bot_message = f"{str(error)}"
+
+            chat_history.append(("user", user_message))
+            chat_history.append(("assistant", bot_message))
+            return "", chat_history
+
+        return responder
+
+    def get_responder(self):
+        """Returns the responder function."""
+        if self.backend_type == "agent":
+            return self.get_agent_responder()
+        elif self.backend_type == "llm":
+            return self.get_llama_responder()
+        else:
+            raise ValueError(f"Unknown bself.ackend type: {self.backend_type}")
+
+    def _dict_to_tuple(self, chat_history):
+        """Convert a list of dictionaries to a list of tuples."""
+        return [(list(item.keys())[0], list(item.values())[0]) for item in chat_history]
+
+    def _tuple_to_dict(self, chat_history):
+        """Convert a list of tuples to a list of dictionaries."""
+        return [{"role": item[0], "content": item[1]} for item in chat_history]
