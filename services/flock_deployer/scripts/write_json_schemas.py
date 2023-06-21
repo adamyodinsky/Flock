7590eb7e36975b5daa4b3170f6c9335b840d89ba
adam.promo@gmail.com <27074934+adamyodinsky@users.noreply.github.com>
2023-06-21 12:14:53 +0300

    refactored write json schemas

--- services/flock_deployer/scripts/write_json_schemas.py
+++ services/flock_deployer/scripts/write_json_schemas.py
@@ -11,24 +11,26 @@ if not os.path.exists(PATH):
     os.makedirs(PATH)
 
 
-def write_schemas(schemas):  # pylint: disable=missing-function-docstring
-    if not os.path.exists(f"{PATH}"):
-        os.makedirs(f"{PATH}")
+def write_schemas(schemas, sub_path: str):
+    if not os.path.exists(f"{PATH}/{sub_path}"):
+        os.makedirs(f"{PATH}/{sub_path}")
 
     for name, schema in schemas.items():
-        print(f"Writing {name} - OK")
+        print(f"Writing {name} - ", end="", flush=True)
         schema_json = schema.schema()
-
         # write to file
-        with open(f"{PATH}/{name}.json", "w", encoding="utf-8") as json_file:
+        with open(f"{PATH}/{sub_path}/{name}.json", "w", encoding="utf-8") as json_file:
             json.dump(schema_json, json_file, indent=2)
+        print("OK")
 
 
 def run_script():
     """Main function."""
     deployment_schema_factory = DeploymentSchemaFactory()
+    sub_schemas = deployment_schema_factory.sub_schemas
     schemas = deployment_schema_factory.schemas
-    write_schemas(schemas)
+    write_schemas(sub_schemas, "sub")
+    write_schemas(schemas, "main")
 
 
 if __name__ == "__main__":
