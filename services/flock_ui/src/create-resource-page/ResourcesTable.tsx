d0e4101cce0b8c95a73d6570c11abd0d10bf83ca
adam.promo@gmail.com <27074934+adamyodinsky@users.noreply.github.com>
2023-12-29 19:26:25 +0200

    flock ui - table

---
+++ services/flock_ui/src/create-resource-page/ResourcesTable.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ResourcesTable = () => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th></th>
+        </tr>
+      </thead>
+    </table>
+  );
+};
+
+export default ResourcesTable;
