3f4cbb198299f4bf0207596c769bd13894c4f68d
adam.promo@gmail.com <27074934+adamyodinsky@users.noreply.github.com>
2023-12-29 19:26:11 +0200

    flock ui and aligning the project - wip table

--- services/flock_ui/src/App.tsx
+++ services/flock_ui/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import CreateResourcePage from "./create-resource-page/CreateResourcePage";
+import ResourcesTable from "./create-resource-page/ResourcesTable";
 
 function App() {
   return (
     <>
-      <CreateResourcePage />
+      {/* <CreateResourcePage /> */}
+      <ResourcesTable />
     </>
   );
 }
