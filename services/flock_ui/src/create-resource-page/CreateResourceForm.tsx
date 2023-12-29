aa7cce9b95a41634c74a8d8e58c821b164320964
adam.promo@gmail.com <27074934+adamyodinsky@users.noreply.github.com>
2023-12-29 13:58:41 +0200

    flock ui and aligning the project - wip

--- services/flock_ui/src/create-resource-page/CreateResourceForm.tsx
+++ services/flock_ui/src/create-resource-page/CreateResourceForm.tsx
@@ -1,12 +1,77 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { ResourceFormData, resourceFormSchema } from "./schemas";
+
 const CreateResourceForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<ResourceFormData>({ resolver: zodResolver(resourceFormSchema) });
+
   return (
     <form className="form-control">
       <div className="mb-3">
         <label className="form-label" htmlFor="name">
           Name
         </label>
-        <input type="text" className="form-control"></input>
+        <input
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        ></input>
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="description">
+          Description
+        </label>
+        <input
+          {...register("description")}
+          id="description"
+          type="text"
+          className="form-control"
+        ></input>
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
+      </div>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="namespace">
+          Namespace
+        </label>
+        <select
+          {...register("namespace")}
+          id="namespace"
+          className="form-control"
+        >
+          {/* this should be fetched dynamically */}
+          <option value="default">default</option>
+        </select>
+        {errors.namespace && (
+          <p className="text-danger">{errors.namespace.message}</p>
+        )}
+      </div>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="kind">
+          Kind
+        </label>
+        <select {...register("kind")} id="kind" className="form-control">
+          {/* this should be fetched dynamically */}
+          <option value="default">default</option>
+        </select>
+        {errors.kind && <p className="text-danger">{errors.kind.message}</p>}
+      </div>
+      <button
+        disabled={!isValid}
+        onClick={handleSubmit((data) => {
+          console.log(data);
+        })}
+        className="btn btn-primary"
+      >
+        Submit
+      </button>
     </form>
   );
 };
--- services/flock_ui/src/create-resource-page/schemas.ts
+++ services/flock_ui/src/create-resource-page/schemas.ts
@@ -1,5 +1,53 @@
-import { array, string, z } from "zod";
+import { string, z } from "zod";
 
+export interface BaseMetaData {
+  annotations: { [key: string]: string };
+  labels: { [key: string]: string };
+  name: string;
+  description: string;
+}
+
+export interface BaseToolDependency {
+  labels: { [key: string]: string };
+  name: string;
+  kind: "Embedding" | "VectorStore" | "VectorStoreQATool" | "LLM" | "LLMChat" | "LoadTool" | "Splitter" | "Agent" | "PromptTemplate" | "LLMTool" | "EmbeddingsLoader" | "WebScraper" | "CSVTool" | "BrowserTool" | "Custom";
+  namespace: string;
+  options: Record<string, any>;
+  description: string;
+}
+
+export interface BaseSpec {
+  options: Record<string, any>;
+  vendor: string;
+  tools: BaseToolDependency[];
+  dependencies: BaseToolDependency[];
+}
+
+export interface BaseResourceSchema {
+  id: string;
+  apiVersion: "flock/v1";
+  kind: "Embedding" | "VectorStore" | "VectorStoreQATool" | "LLM" | "LLMChat" | "LoadTool" | "Splitter" | "Agent" | "PromptTemplate" | "LLMTool" | "EmbeddingsLoader" | "WebScraper" | "CSVTool" | "BrowserTool" | "Custom";
+  category: "other" | "tool" | "scraper" | "model" | "agent" | "deployment" | "job" | "cronjob" | "statefulset";
+  namespace: string;
+  metadata: BaseMetaData;
+  created_at: string;
+  updated_at: string;
+  spec: BaseSpec;
+}
+
+
+export const BaseToolDependencySchema = z.object({
+  labels: z.record(z.string()),
+  name: z.string(),
+  kind: z.enum([
+    'Embedding', 'VectorStore', 'VectorStoreQATool', 'LLM', 'LLMChat', 'LoadTool',
+    'Splitter', 'Agent', 'PromptTemplate', 'LLMTool', 'EmbeddingsLoader', 'WebScraper',
+    'CSVTool', 'BrowserTool', 'Custom'
+  ]),
+  namespace: z.string(),
+  options: z.record(z.string()),
+  description: z.string(),
+});
 
 export const resourceFormSchema = z.object({
   name: z.string().max(64),
@@ -7,17 +55,8 @@ export const resourceFormSchema = z.object({
   namespace: z.string().default("default"),
   kind: string().max(64),
   vendor: string().max(64),
-  options: z.record(z.string()),
-  dependencies: array(z.object({
-    name: string(),
-    kind: string(),
-    namespace: string()
-  })),
-  tools: array(z.object({
-    name: string(),
-    kind: string(),
-    namespace: string()
-  })),
+  options: z.array(BaseToolDependencySchema),
+  tools: z.array(BaseToolDependencySchema),
 });
 
 export const fieldNames = Object.keys(resourceFormSchema.shape);
