57b8e068e58c1af3421d8bb2aa7cfda2c2e0cf86
adam.promo@gmail.com <27074934+adamyodinsky@users.noreply.github.com>
2024-01-08 23:44:59 +0100

    flock_ui - api service

---
+++ services/flock_ui/src/services/deployments_api.ts
@@ -0,0 +1,75 @@
+import axios, { CanceledError } from "axios";
+import { BaseResourceSchema } from "../schemas";
+
+const HOST = process.env.DEPLOYER_SERVER_HOST || "localhost";
+const PORT = process.env.DEPLOYER_SERVER_PORT || 9900;
+
+const apiClient = axios.create({
+  baseURL: `http://${HOST}:${PORT}`,
+})
+
+export { CanceledError };
+
+
+export interface ResourceParams {
+  category?: string;
+  kind?: string;
+  name?: string;
+  namespace?: string;
+  id?: number;
+}
+
+export class ResourceSchemaService {
+  getAll() {
+    const controller = new AbortController();
+    const request = apiClient
+      .get("/schemas", {
+        signal: controller.signal,
+      })
+
+    return { request, cancel: () => controller.abort() }
+  }
+
+  get(kind: string) {
+    return apiClient
+      .get(`/schema/${kind}`)
+  }
+}
+
+
+export class ResourceService {
+
+  getAll(params: ResourceParams) {
+    const controller = new AbortController();
+
+    const request = apiClient
+      .get("/resources", {
+        signal: controller.signal, params: params
+      })
+
+    return { request, cancel: () => controller.abort() }
+  }
+
+  get(params: ResourceParams) {
+
+    return apiClient
+      .get("/resource", { params: params })
+  }
+
+  post(resource: BaseResourceSchema) {
+    return apiClient.post("/resource", resource, {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+  }
+
+  delete(id: string) {
+    return apiClient.delete(`/resource/${id}`)
+  }
+
+  put(resource: BaseResourceSchema) {
+    return apiClient.put(`/resource/${resource.id}`, resource)
+  }
+}
+
